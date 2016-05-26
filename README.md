d0d100e1d40eded8b87680147552a3846d471bff
Steve <stg7@gmx.de>
2016-05-26 22:38:20 +0200

    initial commit of first files

---
+++ README.md
@@ -0,0 +1,17 @@
+traceview
+=========
+
+Traceview is a small tool for visualisation of different traceroute calls.
+
+The idea is based on a software project (Vitranet) that I built with 7 other peoples a long time ago during our
+computer science study. Vitranet was just a small prototype, and no one published or maintained it for years, thats
+why I created traceview.
+
+Requirements
+------------
+You need:
+
+* scons
+* g++/c++ compiler and build tools
+
+
---
+++ prepare.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#   Script for downloading additional third-party libs
+#
+#   This file is part of TraceView.
+#
+#   TraceView is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   TraceView is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with TraceView.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+#    Printout error message.
+#
+logError() {
+    echo -e "\033[91m[ERROR]\033[0m $@ " 1>&2;
+}
+
+#
+#    Printout info message.
+#
+logInfo() {
+    echo -e "\033[92m[INFO ]\033[0m $@"
+}
+
+cpplint() {
+    if [ -f "cpplint.py" ]; then
+        logInfo "cpplint.py already installed locally"
+        return
+    fi
+    logInfo "cpplint.py download"
+    wget "https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py"
+}
+
+boost() {
+    if [ -d "boost" ]; then
+        logInfo "boost is already installed locally"
+        return
+    fi
+    logInfo "boost download"
+    wget -c "http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download"
+    mv "download" "boost_1_60_0.tar.bz2"
+    tar -jxvf "boost_1_60_0.tar.bz2"
+    mv "boost_1_60_0" "boost"
+    cd boost
+    mkdir build
+    ./bootstrap.sh --prefix=./build
+    ./b2 install
+    cd ..
+}
+
+
+mkdir -p libs
+cd libs
+boost
+cd ..
+
+mkdir -p tools
+cd tools
+cpplint
+cd ..
