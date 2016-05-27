d414e2bdd2735cc53811b5c36e527fa9543069db
Steve <stg7@gmx.de>
2016-05-27 02:13:37 +0200

    add tracer things

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -1,10 +1,16 @@
-
 #include <iostream>
 
+#include "tracer.hpp"
 /**
     TraceView: command line tracetool
 **/
 int main(int argc, const char* argv[]) {
-    std::cout << "hello" << std::endl;
+    traceview::Tracer t;
+
+    // todo(stg7) read list of hosts from file, and start multiple threads for getting all traces
+    std::cout << "hello "
+        << t.trace("google.de")
+        << std::endl;
+
     return 0;
 }
---
+++ src/tracer.hpp
@@ -0,0 +1,40 @@
+
+
+#ifndef TRACER_H
+#define TRACER_H
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+
+namespace traceview {
+
+    std::string ssystem(const char* cmd) {
+        char buffer[128];
+        std::string result = "";
+        std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+        if (!pipe) throw std::runtime_error("popen() failed!");
+        while (!feof(pipe.get())) {
+            if (fgets(buffer, 128, pipe.get()) != NULL)
+                result += buffer;
+        }
+        return result;
+    }
+
+    class Tracer {
+     public:
+        std::string trace(const std::string& url) {
+            std::stringstream command;
+            command << "traceroute " << url;
+            auto res = ssystem(command.str().c_str());
+            // todo(stg7) do some string parsing of output
+            return res;
+        }
+    };
+
+}
+#endif
